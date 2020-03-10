29dce7200f2fdd9d194e25e2a83daa5775e2b8d3
edd23 <yahired3@gmail.com>
2020-03-09 22:02:11 -0600

    last commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,55 @@
-import React, {Component} from 'react';
-import "./App.css";
+import React, { Component } from 'react';
+import './App.css';
 import Header from './components/header';
 import TodoInput from './components/todoInput';
+import TodoItem from './components/todoItem';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        {id: 0, text: "Lettuce"},
+        {id: 1, text: "Tomatoes"},
+        {id: 2, text: "Milk"}
+      ],
+      nextId: 3
+    };
+
+    this.addTodo = this.addTodo.bind(this);
+    this.removeTodo = this.removeTodo.bind(this);
+  }
+
+  addTodo(todoText) {
+    let todos = this.state.todos.slice();
+    todos.push({id: this.state.nextId, text: todoText});
+    this.setState({
+      todos: todos,
+      nextId: ++this.state.nextId
+    });
+  }
+  
+
+  removeTodo(id) {
+    this.setState({
+        todos: this.state.todos.filter((todo, index) => todo.id !== id)
+      });
+  }
+
   render() {
     return (
       <div className="App">
-       <div className="todo-wrapper">
-         <Header />
-         <TodoInput />
-       </div>
+        <div className="todo-wrapper">
+          <Header />
+          <TodoInput todoText="" addTodo={this.addTodo} />
+          <ul>
+            {
+              this.state.todos.map((todo) => {
+                return <TodoItem todo={todo} key={todo.id} id={todo.id} removeTodo={this.removeTodo}/>
+              })
+            }
+          </ul>
+        </div>
       </div>
     );
   }
--- src/components/todoInput.js
+++ src/components/todoInput.js
@@ -1,29 +1,35 @@
 import React from 'react';
+import './todoInput.css';
 
 export default class TodoInput extends React.Component {
   constructor(props) {
-    super(props)
-
-    this.state = {value: "test"};
+    super(props);
+    this.state = {value: this.props.todoText};
 
     this.handleChange = this.handleChange.bind(this);
     this.addTodo = this.addTodo.bind(this);
   }
 
   handleChange(e) {
-    console.log("change here");
+    this.setState({value: e.target.value});
   }
 
   addTodo(todo) {
-    console.log("TODO: ", todo);
+    // Ensure a todo was actually entered before submitting
+    if (todo.length > 0) {
+      this.props.addTodo(todo);
+      this.setState({value: ''});
+    }
   }
+  
+  
 
   render() {
     return (
       <div>
-        <input type="text" values="" onChange={this.handleChange} />
+        <input type="text" value={this.state.value} onChange={this.handleChange} />
         <button className="btn btn-primary" onClick={() => this.addTodo(this.state.value)}>Submit</button>
       </div>
-    )
+    );
   }
 }
